8c53a3afb3e59c3328a375ca7effa3b8a956bbdc
crazyacking <crazyacking@gmail.com>
2017-07-25 20:39:42 +0800

    impl ApnsPooledConnection.java

--- commit.sh
+++ commit.sh
@@ -2,11 +2,12 @@
 
 echo "begin commit"
 
+sleep 30
 
 date "+%Y-%m-%d %H:%M:%S" >> exe_history
 
 git add .
-git commit -m "update"
+git commit -m "impl ApnsPooledConnection.java"
 git push
 
 echo "end commit"
