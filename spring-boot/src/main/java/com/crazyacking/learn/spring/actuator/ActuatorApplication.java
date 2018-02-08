4c21d9e204e3bddcc3cd5215c494ec03918a8e85
shanbiao.jsb <shanbiao.jsb@alibaba-inc.com>
2018-02-08 14:42:12 +0800

    impl spring-boot-actuator

--- spring-boot/src/main/java/com/crazyacking/learn/spring/actuator/ActuatorApplication.java
+++ spring-boot/src/main/java/com/crazyacking/learn/spring/actuator/ActuatorApplication.java
@@ -1,14 +1,14 @@
-package com.crazyacking.learn.spring.actuator;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-
-@SpringBootApplication
-@Configuration
-public class ActuatorApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ActuatorApplication.class, args);
-    }
-}
+//package com.crazyacking.learn.spring.actuator;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.Configuration;
+//
+//@SpringBootApplication
+//@Configuration
+//public class ActuatorApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(ActuatorApplication.class, args);
+//    }
+//}
